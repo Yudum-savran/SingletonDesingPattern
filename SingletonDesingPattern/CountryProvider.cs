e96a54f32c6b58ed61db5d77c2c63ec4b72003ff
yudum-savran <83405737+Yudum-savran@users.noreply.github.com>
2023-08-11 13:11:30 +0300

    Add project files.

---
+++ SingletonDesingPattern/CountryProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesingPattern
+{
+	public class CountryProvider
+	{
+		private static CountryProvider instance;
+		public static CountryProvider Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new CountryProvider();	
+				}
+				return instance;
+			}
+			set=> instance = value;
+		
+		}
+
+		private new List<Country> Countries { get; set; }
+
+		public async Task<List<Country>> GetCountriesAsync()
+		{
+			if(Countries is null)
+			{
+				Countries = new List<Country>()
+				{
+					new Country() {CountryName="Türkiye"},
+					new Country() {CountryName="Almanya"}
+				};
+			}
+
+			return Countries;
+		}
+			
+	}
+}
---
+++ SingletonDesingPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using SingletonDesingPattern;
+
+var countries = await CountryProvider.Instance.GetCountriesAsync();
+
+foreach (var country in countries)
+{
+	Console.WriteLine(country.CountryName);
+}
+
+//baska servisten çağrılırsa
+var country1 = await CountryProvider.Instance.GetCountriesAsync();
+Console.WriteLine(country1);
